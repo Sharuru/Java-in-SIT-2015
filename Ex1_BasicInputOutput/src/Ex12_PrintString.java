f06399b5d329f57ce92742f4f96f186570d2aa08
Sharuru <mave@foxmail.com>
2015-04-03 19:13:32 +0800

    Finish Ex12

---
+++ Ex1_BasicInputOutput/src/Ex12_PrintString.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Mave on 2015/4/3 0003.
+ * Page 58 Question 3: Print the following sentence using String:'Java is an object oriented programming language.'.
+ */
+public class Ex12_PrintString {
+    public static void main(String[] args) {
+        String sentence = "Java is an object oriented programming language.";
+        System.out.println(sentence);
+    }
+}
