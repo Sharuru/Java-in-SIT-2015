93ae95a41a62b952aee6ea70447f079045cba708
Sharuru <mave@foxmail.com>
2015-04-17 15:21:24 +0800

    Create Ex51

---
+++ Ex5_GUI/src/Ex51_Notepad.java
@@ -0,0 +1,27 @@
+import java.awt.*;
+import javax.swing.*;
+/*
+ * Created by JFormDesigner on Fri Apr 17 15:21:02 CST 2015
+ */
+
+
+
+/**
+ * @author Sharuru
+ */
+public class Ex51_Notepad extends JPanel {
+    public Ex51_Notepad() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+
+        //======== this ========
+        setLayout(new BorderLayout());
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
+    // JFormDesigner - End of variables declaration  //GEN-END:variables
+}
