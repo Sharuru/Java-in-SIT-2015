f2845d6822f43e59c7387872fa160a53e21476fb
Sharuru <mave@foxmail.com>
2015-06-24 00:11:04 +0800

    Complete UI

--- Design_NetworkMonitor/src/MainForm.java
+++ Design_NetworkMonitor/src/MainForm.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+
 /**
  * Created by Sharuru on 2015/6/23 0023.
  */
 public class MainForm {
+    private JComboBox comboBoxNICs;
+    private JButton buttonAction;
+    private JLabel labelGuide1;
+    private JPanel panelBase;
+    private JLabel labelGuide2;
+    private JLabel labelGuide3;
+    private JLabel labelUploadSpeed;
+    private JLabel labelDownloadSpeed;
+    private JScrollPane scrollPanelLogs;
+    private JTextArea textAreaLogs;
 }
