7f8b128beffbbdf05d5b58920ac89f7678743a36
Sharuru <mave@foxmail.com>
2015-06-24 00:25:56 +0800

    Auto generated main code

--- Design_NetworkMonitor/src/MainForm.java
+++ Design_NetworkMonitor/src/MainForm.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Sharuru on 2015/6/23 0023.
@@ -14,4 +15,22 @@ public class MainForm {
     private JLabel labelDownloadSpeed;
     private JScrollPane scrollPanelLogs;
     private JTextArea textAreaLogs;
+
+    public MainForm() {
+    }
+
+    public static void main(String[] args) {
+        //Auto generated
+        JFrame frame = new JFrame("MainForm");
+        frame.setContentPane(new MainForm().panelBase);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        //Custom set
+        frame.setTitle("Net Monitor");
+        frame.setMinimumSize(new Dimension(545, 245));
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
+
+
