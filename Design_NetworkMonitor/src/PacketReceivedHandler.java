b9e208dcad7d857bd4d368cc735bc417490cc9e3
Sharuru <mave@foxmail.com>
2015-06-24 13:22:58 +0800

    PacketReceivedHandler

---
+++ Design_NetworkMonitor/src/PacketReceivedHandler.java
@@ -0,0 +1,24 @@
+import jpcap.PacketReceiver;
+import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
+import jpcap.packet.UDPPacket;
+
+/**
+ * Created by Sharuru on 2015/6/24 0024.
+ */
+public class PacketReceivedHandler implements PacketReceiver{
+
+
+    @Override
+    public void receivePacket(Packet packet) {
+        MainForm Mhandler = new MainForm();
+        if(packet instanceof jpcap.packet.TCPPacket){
+            TCPPacket tp = (TCPPacket)packet;
+            Mhandler.updateLog("[Get TCP] From:" + tp.src_ip + ":" + tp.src_port + " To: " + tp.dst_ip + ":" + tp.dst_port + ". Length = " + tp.len);
+        }
+        else if(packet instanceof jpcap.packet.UDPPacket){
+            UDPPacket tp = (UDPPacket)packet;
+            Mhandler.updateLog("[Get UDP] From:" + tp.src_ip + ":" + tp.src_port + " To: " + tp.dst_ip + ":" + tp.dst_port + ". Length = " + tp.len);
+        }
+    }
+}
