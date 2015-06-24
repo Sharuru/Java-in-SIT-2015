4e35e2bf519238b95289703320d5870ffcc2933f
Sharuru <mave@foxmail.com>
2015-06-24 18:08:15 +0800

    Cleanup code

--- Design_NetworkMonitor/src/NetworkHandler.java
+++ Design_NetworkMonitor/src/NetworkHandler.java
@@ -11,15 +11,29 @@ import java.io.IOException;
  * Created by Sharuru on 2015/6/24 0024.
  */
 public class NetworkHandler {
+    protected Thread captureThread;
     JpcapCaptor cap = null;
     MainForm frame;
+    protected PacketReceiver handler = new PacketReceiver() {
+        public void receivePacket(Packet packet) {
+            if (packet instanceof jpcap.packet.TCPPacket) {
+                TCPPacket tp = (TCPPacket) packet;
+                frame.dealPacket("[Get TCP] From:" + tp.src_ip.toString().substring(1, tp.src_ip.toString().length() - 1) + ":" + tp.src_port
+                        + " -> To: " + tp.dst_ip.toString().substring(1, tp.dst_ip.toString().length() - 1) + ":" + tp.dst_port + ". Length = " + tp.len);
+            } else if (packet instanceof jpcap.packet.UDPPacket) {
+                UDPPacket up = (UDPPacket) packet;
+                frame.dealPacket("[Get UDP] From:" + up.src_ip.toString().substring(1, up.src_ip.toString().length() - 1) + ":" + up.src_port
+                        + " -> To: " + up.dst_ip.toString().substring(1, up.dst_ip.toString().length() - 1) + ":" + up.dst_port + ". Length = " + up.len);
+            }
+        }
+    };
 
     protected void setMainForm(MainForm frame) {
         this.frame = frame;
     }
 
     protected void capturePacketFromDevice(int deviceIndex) {
-        if (cap != null){
+        if (cap != null) {
             cap.close();
         }
         DevicesHandler Dhandler = new DevicesHandler();
@@ -34,8 +48,6 @@ public class NetworkHandler {
             startCaptureThread();
         }
     }
-
-    protected Thread captureThread;
 
     protected void startCaptureThread() {
         if (captureThread != null) {
@@ -57,18 +69,4 @@ public class NetworkHandler {
     protected void stopCapture() {
         stopCaptureThread();
     }
-
-    protected PacketReceiver handler = new PacketReceiver() {
-        public void receivePacket(Packet packet) {
-            if (packet instanceof jpcap.packet.TCPPacket) {
-                TCPPacket tp = (TCPPacket) packet;
-                frame.dealPacket("[Get TCP] From:" + tp.src_ip.toString().substring(1, tp.src_ip.toString().length() - 1) + ":" + tp.src_port
-                        + " -> To: " + tp.dst_ip.toString().substring(1, tp.dst_ip.toString().length() - 1) + ":" + tp.dst_port + ". Length = " + tp.len);
-            } else if (packet instanceof jpcap.packet.UDPPacket) {
-                UDPPacket up = (UDPPacket) packet;
-                frame.dealPacket("[Get UDP] From:" + up.src_ip.toString().substring(1, up.src_ip.toString().length() - 1) + ":" + up.src_port
-                        + " -> To: " + up.dst_ip.toString().substring(1, up.dst_ip.toString().length() - 1) + ":" + up.dst_port + ". Length = " + up.len);
-            }
-        }
-    };
 }
