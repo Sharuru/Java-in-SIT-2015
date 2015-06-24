46b6d2e55a31c3c65d1ddaa0cb0446ba679506b6
Sharuru <mave@foxmail.com>
2015-06-24 12:35:33 +0800

    initDeviceList()

--- Design_NetworkMonitor/src/MainForm.java
+++ Design_NetworkMonitor/src/MainForm.java
@@ -1,5 +1,11 @@
+import jpcap.NetworkInterface;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 /**
  * Created by Sharuru on 2015/6/23 0023.
@@ -16,9 +22,6 @@ public class MainForm {
     private JScrollPane scrollPanelLogs;
     private JTextArea textAreaLogs;
 
-    public MainForm() {
-    }
-
     public static void main(String[] args) {
         //Auto generated
         JFrame frame = new JFrame("MainForm");
@@ -30,6 +33,24 @@ public class MainForm {
         frame.setMinimumSize(new Dimension(545, 245));
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+    }
+
+    public MainForm() {
+        initDeviceList();
+        comboBoxNICs.addActionListener(e -> selectedNICUpdate());
+    }
+
+    private void initDeviceList(){
+        Models handler = new Models();
+        NetworkInterface[] devices = handler.listDevices();
+        for(int i =0;i<devices.length;i++){
+            comboBoxNICs.addItem(devices[i].description);
+        }
+        selectedNICUpdate();
+    }
+
+    private void selectedNICUpdate(){
+        textAreaLogs.append("Current selected NIC is: " + comboBoxNICs.getSelectedItem() + "\n");
     }
 }
 
---
+++ Design_NetworkMonitor/src/Models.java
@@ -0,0 +1,22 @@
+import jpcap.JpcapCaptor;
+import jpcap.NetworkInterface;
+import jpcap.PacketReceiver;
+import jpcap.packet.Packet;
+
+/**
+ * Created by Sharuru on 2015/6/24 0024.
+ */
+public class Models {
+    public NetworkInterface[] listDevices(){
+        try{
+            final NetworkInterface[] devices = JpcapCaptor.getDeviceList();
+            return devices;
+        }
+        catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new NetworkInterface[0];
+    }
+
+
+}
