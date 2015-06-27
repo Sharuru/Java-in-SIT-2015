73122a1d219a5083cd1caf910e11dcf27619c9ad
Sharuru <mave@foxmail.com>
2015-06-27 21:33:20 +0800

    Use Queue
    Waste RAM but save CPUs

--- Design_NetworkMonitor/src/ChartHandler.java
+++ Design_NetworkMonitor/src/ChartHandler.java
@@ -2,43 +2,45 @@ import org.jfree.data.xy.XYDataset;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * Created by Sharuru on 2015/6/26 0026.
  */
 public class ChartHandler {
-    protected static ArrayList<Double> tcpA = new ArrayList<>();
-    protected static ArrayList<Double> udpA = new ArrayList<>();
+    protected static Queue<Double> tcpQ = new LinkedList<>();
+    protected static Queue<Double> udpQ = new LinkedList<>();
     protected static int space;
 
     protected static void initSpeedArrayList(int space) {
         //Something like malloc in C
         ChartHandler.space = space;
         for (int i = 0; i < space; i++) {
-            tcpA.add(i, 0.0);
-            udpA.add(i, 0.0);
+            tcpQ.offer(0.0);
+            udpQ.offer(0.0);
         }
     }
 
     protected static XYDataset createDataset(double newTcpSpeed, double newUdpSpeed) {
         XYSeries tcpDS = new XYSeries("TCP");
         XYSeries udpDS = new XYSeries("UDP");
-        tcpA.set(space - 1, newTcpSpeed);
-        udpA.set(space - 1, newUdpSpeed);
+        //Kick the first and add the new speed to the last
+        tcpQ.poll();
+        tcpQ.offer(newTcpSpeed);
+        udpQ.poll();
+        udpQ.offer(newUdpSpeed);
+        //Copy queue for dataset uses
+        Queue<Double> tcpQQ = new LinkedList<>(tcpQ);
+        Queue<Double> udpQQ = new LinkedList<>(udpQ);
         //Set dataset
         for (int i = 0; i < space; i++) {
-            tcpDS.add(i, tcpA.get(i));
-            udpDS.add(i, udpA.get(i));
+            tcpDS.add(i, tcpQQ.poll());
+            udpDS.add(i, udpQQ.poll());
         }
-        //According to space, loop the array list
-        for (int i = 0; i < space - 1; i++) {
-            tcpA.set(i, tcpA.get(i + 1));
-            udpA.set(i, udpA.get(i + 1));
-        }
-        XYSeriesCollection XYdataset = new XYSeriesCollection();
-        XYdataset.addSeries(tcpDS);
-        XYdataset.addSeries(udpDS);
-        return XYdataset;
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(tcpDS);
+        dataset.addSeries(udpDS);
+        return dataset;
     }
 }
